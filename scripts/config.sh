af16b43f54cec1399cc7e40bb8938a3261f42478
Dave Blanford <dave@daveandval.org>
2014-01-04 10:56:46 -0700

    Update settings

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,22 +1,22 @@
 ## Shell Script Configuration Details ##
 
-# Name of the project. Used for: mysql database name, postfix mailname, 
-project_name='my_project' #required
+# Name of the project. Used for: mysql database name, postfix mailname,
+project_name='vagrant_provision_scripts' #required
 
 # Mysql Root Password
 mysql_password='password' #required
 
 # Package repository used by apt (in etc/apt/sources.list)
 # comment it out to use default repository
-ubuntu_mirror='mirrors.example.com' 
+ubuntu_mirror='mirrors.example.com'
 
 # Timezone used to dispaly date/time info. Server clock is set to UTC
 # comment it out if you don't want to use it
-time_zone='US/Mountain' 
+time_zone='US/Mountain'
 
 # path to sql dump of the database on the guest VM.
 # comment it out if you do not want to install a database from sql dump
-db_path='/vagrant/db' 
+db_path='/vagrant/db'
 
 # email address displayed on apache server errors
 # comment it out if you don't want to use it
