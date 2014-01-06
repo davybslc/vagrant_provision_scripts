3e446f97ae4293792c39e879656665e339ea4ec2
Dave Blanford <dave@daveandval.org>
2014-01-06 13:49:27 -0700

    Change default configuration

--- scripts/config.sh
+++ scripts/config.sh
@@ -8,19 +8,19 @@ mysql_password='password' #required
 
 # Package repository used by apt (in etc/apt/sources.list)
 # comment it out to use default repository
-ubuntu_mirror='mirrors.example.com'
+# ubuntu_mirror='mirrors.example.com'
 
 # Timezone used to dispaly date/time info. Server clock is set to UTC
 # comment it out if you don't want to use it
-time_zone='US/Mountain'
+# time_zone='US/Mountain'
 
 # path to sql dump of the database on the guest VM.
 # comment it out if you do not want to install a database from sql dump
-db_path='/vagrant/db'
+# db_path='/vagrant/db'
 
 # email address displayed on apache server errors
 # comment it out if you don't want to use it
-admin_email='me@example.com'
+# admin_email='me@example.com'
 
 # IP address of the VM host in the VM network, used in xdebug config
 # comment it out if you are not installing xdebug
--- scripts/install_xdebug.sh
+++ scripts/install_xdebug.sh
@@ -15,14 +15,14 @@ then
 	echo ' ' >> /etc/php5/apache2/php.ini
 	echo '[xdebug]' >> /etc/php5/apache2/php.ini
 
-	# append xdebug location to php.ini
+	# append the following configurations
 	echo "zend_extension=$xdebug_path" >> /etc/php5/apache2/php.ini
-
-	# append remote enabled to php.ini
-	echo 'xdebug.remote_enable=1' >> /etc/php5/apache2/php.ini
-
-	# append remote enabled to php.ini
 	echo "xdebug.remote_host=$vm_host_ip" >> /etc/php5/apache2/php.ini
+	echo 'xdebug.remote_enable=1' >> /etc/php5/apache2/php.ini
+	echo 'xdebug.remote_autostart=1' >> /etc/php5/apache2/php.ini
+	echo 'xdebug.remote_port=9000' >> /etc/php5/apache2/php.ini
+	echo 'xdebug.remote_handler=dbgp' >> /etc/php5/apache2/php.ini
+
 
 	# Restart apache
 	/etc/init.d/apache2 restart
